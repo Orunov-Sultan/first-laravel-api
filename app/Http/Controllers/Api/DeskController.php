79ab86937ad78c7bd243fc3eb9a16a1f90a03370
Sultan-home <sultat.orunov@mail.ru>
2023-02-04 00:25:30 +0500

    adding data and validation

--- app/Http/Controllers/Api/DeskController.php
+++ app/Http/Controllers/Api/DeskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Desk\StoreRequest;
 use App\Http\Resources\DeskResource;
 use App\Models\Desk;
 use Illuminate\Http\Request;
@@ -27,9 +28,10 @@ class DeskController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreRequest $request)
     {
-        //
+        $createdDesk = Desk::create($request->validated());
+        return new DeskResource($createdDesk);
     }
 
     /**
